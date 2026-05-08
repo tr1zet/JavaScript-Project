3dd9fab9b560144aec91e24a026c2915298f1846
tr1zet <uliskin06@mail.ru>
2026-05-08 22:35:31 +0700

    Basic JavaScript Modules

---
+++ js/models/Country.js
@@ -0,0 +1,15 @@
+import { getRussianName, getRussianRegion } from '../data/russianNames.js';
+
+export default class Country {
+  constructor(data) {
+    this.englishName = data.name.common;
+    this.russianName = getRussianName(this.englishName);
+    this.name = `${this.russianName} (${this.englishName})`; // 👈 ВОТ ЭТО ВАЖНО!
+    this.flag = data.flags.svg;
+    this.capital = data.capital?.[0] || 'Нет данных';
+    this.population = data.population || 0;
+    this.area = data.area || 0;
+    this.region = getRussianRegion(data.region || 'Other');
+    this.languages = data.languages ? Object.values(data.languages) : [];
+  }
+}
