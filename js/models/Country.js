23eb171ba3946486627205ec56fa0df3a3d3d504
tr1zet <148443756+tr1zet@users.noreply.github.com>
2026-05-08 22:36:24 +0700

    Update Country.js

--- js/models/Country.js
+++ js/models/Country.js
@@ -4,7 +4,7 @@ export default class Country {
   constructor(data) {
     this.englishName = data.name.common;
     this.russianName = getRussianName(this.englishName);
-    this.name = `${this.russianName} (${this.englishName})`; // 👈 ВОТ ЭТО ВАЖНО!
+    this.name = `${this.russianName} (${this.englishName})`; 
     this.flag = data.flags.svg;
     this.capital = data.capital?.[0] || 'Нет данных';
     this.population = data.population || 0;
